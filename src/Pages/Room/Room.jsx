ba0cc04c50306b72e1584e9e1e3bfbb7a57711e5
Halima2005 <halimayasmin6579@gmail.com>
2024-08-25 13:16:56 +0530

    version 1 updated

--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -1,35 +1,36 @@
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
 const Room = () => {
-    const { roomId }= useParams();
+    const { roomId } = useParams();
+
     const myMeeting = async (element) => {
-        const appID =1665625276;
-        const serverSecret ="2f1b260997d6c111850d2194e7f521a0";
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,roomId,Date.now().toString(),"Halima Yasmin");
+        const appID = 1665625276;
+        const serverSecret = "2f1b260997d6c111850d2194e7f521a0";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Halima Yasmin");
+
         const zc = ZegoUIKitPrebuilt.create(kitToken);
         zc.joinRoom({
-            container:element,
-            sharedLinks:[
-                        {
-                            name:'Copy Link',
-                            url: `http://localhost:3000/room/.${roomId}`,
-                        }
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Copy Link',
+                    url: `http://localhost:3000/room/.${roomId}`,
+                }
             ],
-            
-            scenario:{
-                mode:ZegoUIKitPrebuilt.OneONoneCall,
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
             },
-            showScreenSharingButton:false,
+            showScreenSharingButton: false,
         });
     };
 
-  return (
-   <div>
-        <div ref={myMeeting}/>
-   </div>
-   
-  )
+    return (
+        <div style={{ width: '100vw', height: '100vh' }}>
+            <div ref={myMeeting} style={{ width: '100%', height: '100%' }} />
+        </div>
+    );
 }
 
-export default Room
+export default Room;
