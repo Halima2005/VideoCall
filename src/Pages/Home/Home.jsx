f95173cec11c5fe10bdb9e0cc804369b6567fd92
Halima2005 <halimayasmin6579@gmail.com>
2025-02-15 13:28:39 +0530

    Your commit message here

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,34 +1,36 @@
-import React, { useCallback, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
-import video from './video.mp4';
+import React, { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
+import video from "./video.mp4";
 const Home = () => {
-    const [value, setValue] = useState('');
-    const navigate = useNavigate();
+  const [value, setValue] = useState("");
+  const navigate = useNavigate();
 
-    const handleJoinRoom = useCallback(() => {
-        navigate(`/room/${value}`);
-    }, [navigate, value]);
+  const handleJoinRoom = useCallback(() => {
+    navigate(`/room/${value}`);
+  }, [navigate, value]);
 
-    return (
-        <div className='home-container'>
-             <h2 className='logo'>Bhasa-Setu</h2> 
-            {/* Background Video */}
-            <video autoPlay loop muted playsInline className='background-video'>
-                <source src={video} type="video/mp4" />
-            </video>
-            <div className='content'>
-                <input 
-                    className='bcentered-button' 
-                    value={value} 
-                    onChange={(e) => setValue(e.target.value)} 
-                    type="text" 
-                    placeholder="Enter Room code" 
-                />
-                <button onClick={handleJoinRoom} className='centered-button'>Join</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="home-container">
+      <h2 className="logo">Video Call</h2>
+      {/* Background Video */}
+      <video autoPlay loop muted playsInline className="background-video">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="content">
+        <input
+          className="bcentered-button"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+          placeholder="Enter Room code"
+        />
+        <button onClick={handleJoinRoom} className="centered-button">
+          Join
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -1,36 +1,42 @@
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Room = () => {
-    const { roomId } = useParams();
+  const { roomId } = useParams();
 
-    const myMeeting = async (element) => {
-        const appID = 1665625276;
-        const serverSecret = "2f1b260997d6c111850d2194e7f521a0";
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Halima Yasmin");
-
-        const zc = ZegoUIKitPrebuilt.create(kitToken);
-        zc.joinRoom({
-            container: element,
-            sharedLinks: [
-                {
-                    name: 'Copy Link',
-                    url: `http://localhost:3000/room/.${roomId}`,
-                }
-            ],
-            scenario: {
-                mode: ZegoUIKitPrebuilt.OneONoneCall,
-            },
-            showScreenSharingButton: false,
-        });
-    };
-
-    return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <div ref={myMeeting} style={{ width: '100%', height: '100%' }} />
-        </div>
+  const myMeeting = async (element) => {
+    const appID = 1787041124;
+    const serverSecret = "eb127ffbc700e56f464636d2cf2c3e13";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now().toString(),
+      "Halima Yasmin"
     );
-}
+
+    const zc = ZegoUIKitPrebuilt.create(kitToken);
+    zc.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Copy Link",
+          url: `http://localhost:3000/room/.${roomId}`,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+      showScreenSharingButton: false,
+    });
+  };
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <div ref={myMeeting} style={{ width: "100%", height: "100%" }} />
+    </div>
+  );
+};
 
 export default Room;
